d490a680cd6c20d11fc17d1e2ef9bb6ecd89059d
adibNo9 <adib.mokhtari@gmail.com>
2022-09-07 17:36:41 +0430

    get users from database.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Signup from "./components/Signup/Signup";
@@ -6,6 +8,22 @@ import Home from "./components/Home/Home";
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+      const getUsers = async () => {
+        const response = await fetch('http://localhost:3001/users');
+
+        const data = await response.json();
+        setUsers(data);
+      };
+
+    getUsers();
+  }, []);
+
+  console.log(users)
+
+
   return (
     <BrowserRouter>
       <Navbar />
